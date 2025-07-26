82e70f0d21812e85c380aa82bda772b72ada6c41
Anwesa Ghosh <anwesa2021@gmail.com>
2025-07-26 13:36:57 +0530

    addcourse added

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useRef, useState} from 'react';
+import Quill from 'quill';
+import uniqid from 'uniqid';
+import { use } from 'react';
 
 const AddCourse = () => {
+
+  const quillRef = useRef(null);
+  const editorRef = useRef(null);
+
+  const [courseTitle, setCourseTitle] =useState('');
+  const [coursePrice, setCoursePrice] =useState(0);
+  const [discount, setDiscount] =useState(0);
+  const [image, setImage] =useState(null);
+  const [chapters, setChapters] =useState([]);
+  const [showPopup, setShowPopup] = useState(false);
+
   return (
-    <div><h1>
-        AddCourse</h1></div>
+    <div>
+
+    </div>
   )
 }
 
