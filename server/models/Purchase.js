6b38ad94850d3db61de2e4f4877771c3b8ec196e
Anwesa Ghosh <anwesa2021@gmail.com>
2025-07-30 13:52:55 +0530

    stripe added

--- server/models/Purchase.js
+++ server/models/Purchase.js
@@ -3,7 +3,7 @@ import Course from "./Course.js";
 import User from "./User.js";
 
 const PurchaseSchema = new mongoose.Schema({
-    CourseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
+    courseId: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true },
     userId: { type: String, ref:'User', required: true },
     amount: { type: Number, required: true },
     status: { type: String, enum: ['pending', 'completed', 'failed'], default: 'pending' },
